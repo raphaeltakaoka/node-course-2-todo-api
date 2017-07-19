89cb01d62b1bdfce8c55bc4e6c913f2c226316d0
Raphael Takaoka <raphaeltakaoka@Raphaels-MacBook-Pro.local>
2017-07-19 14:32:58 -0300

    add authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -26,7 +27,7 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
   var body =  _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
@@ -95,6 +96,11 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
