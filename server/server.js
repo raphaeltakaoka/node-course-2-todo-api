a605e020d05ddfd8c65f88ffa5f99f857c9d582c
Raphael Takaoka <raphaeltakaoka@Raphaels-MacBook-Pro.local>
2017-07-19 16:43:47 -0300

    add delete users token

--- server/server.js
+++ server/server.js
@@ -107,8 +107,18 @@ app.post('/users/login', (req, res) => {
   var body =  _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(body.email, body.password).then((user) => {
-    res.send(user);
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
   }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
     res.status(400).send();
   });
 });
