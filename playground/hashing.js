2a6c4095bf071f4fe05f1f304bdc118ec5c1c0fe
Raphael Takaoka <raphaeltakaoka@Raphaels-MacBook-Pro.local>
2017-07-19 15:10:27 -0300

    add hashed password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 4
-};
-var token = jwt.sign(data, 'somesecret');
-console.log(token);
-var decoded = jwt.verify(token, 'somesecret');
-console.log(decoded);
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword= '$2a$10$1I.hkSCq9kNEO2bIFQ6Ef.GIDpfR86egDS4JUcgyISRMPfCAl5HJC';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 4
+// };
+// var token = jwt.sign(data, 'somesecret');
+// console.log(token);
+// var decoded = jwt.verify(token, 'somesecret');
+// console.log(decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
